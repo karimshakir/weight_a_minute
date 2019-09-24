40d52e3f964461bd1f91d56b0cab84c5df05b62f
“karimshakir” <karimshakir99@gmail.com>
2019-09-24 11:03:52 -0500

    can enroll in a team

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -2,15 +2,26 @@ class EnrollmentsController < ApplicationController
 before_action :authenticate_user
 
   def create
-      enrollment = Enrollment.new(
-                                  player_id: current_user.id,
-                                  team_id: params[:team_id]
-                                  )
+    enrollment = Enrollment.new(
+                                player_id: current_user.id,
+                                team_id: params[:team_id]
+                                )
     if enrollment.save
         render json: {message: 'Enrollment created successfully'}, status: :created
     else
         render json: {errors: enrollment.errors.full_messages}, status: :bad_request
     end
+  end
+
+  def destroy
+    enrollment = Enrollment.destroy(
+      enrollment_id: params[:name]
+    )
+    # if team.save
+    #   render json: {message: 'Team created successfully'}, status: :created
+    # else
+    #   render json: {errors: team.errors.full_messages}, status: :bad_request
+    # end
   end
 end
 
