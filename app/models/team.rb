60793809c1cb91a9ed0553c027a6e0811d6dd56c
“karimshakir” <karimshakir99@gmail.com>
2019-10-05 20:01:28 -0500

    reverse teamlist on Profile component

--- app/models/team.rb
+++ app/models/team.rb
@@ -3,6 +3,7 @@ class Team < ApplicationRecord
   has_many :players, through: :enrollments
 
   validates :name, presence: true, uniqueness: true
+  default_scope { order(created_at: :desc) }
 
   def add_creator_to_team(creator_id)
     Enrollment.create(player_id: creator_id, team_id: id)
@@ -10,7 +11,7 @@ class Team < ApplicationRecord
 
   def my_rank(player_id)
     players
-      .sort_by{ |p| -p.wt_loss }
-      .find_index{ |p| p.id == player_id } + 1
+      .sort_by { |p| -p.wt_loss }
+      .find_index { |p| p.id == player_id } + 1
   end
 end
